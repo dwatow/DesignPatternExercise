01028fe6e96cd5599e90025ef23ab5b94e771282
Kx_wang <Kx_wang@chilinopto.com.tw>
2013-12-26 09:56:09 +0800

    MeasureItem in Decorator add factory
    在MeasureItem加了工廠模式，並且在增加裝飾時使用迴圈的實作，讓使用者決定要增加什麼。

--- Decorator/MeasureItem/MiD25.h
+++ Decorator/MeasureItem/MiD25.h
@@ -7,31 +7,31 @@ class D25 : public MiItem
 {
 	void AddD25(std::vector<std::string>& _vstr) //打光
 	{
-		_vstr.push_back("黑色 1點");
-		_vstr.push_back("黑色 2點");
-		_vstr.push_back("黑色 3點");
-		_vstr.push_back("黑色 4點");
-		_vstr.push_back("黑色 5點");
-		_vstr.push_back("黑色 6點");
-		_vstr.push_back("黑色 7點");
-		_vstr.push_back("黑色 8點");
-		_vstr.push_back("黑色 9點");
-		_vstr.push_back("黑色10點");
-		_vstr.push_back("黑色11點");
-		_vstr.push_back("黑色12點");
-		_vstr.push_back("黑色13點");
-		_vstr.push_back("黑色14點");
-		_vstr.push_back("黑色15點");
-		_vstr.push_back("黑色16點");
-		_vstr.push_back("黑色17點");
-		_vstr.push_back("黑色18點");
-		_vstr.push_back("黑色19點");
-		_vstr.push_back("黑色20點");
-		_vstr.push_back("黑色21點");
-		_vstr.push_back("黑色22點");
-		_vstr.push_back("黑色23點");
-		_vstr.push_back("黑色24點");
-		_vstr.push_back("黑色25點");
+		_vstr.push_back("Dark_ 1");
+		_vstr.push_back("Dark_ 2");
+		_vstr.push_back("Dark_ 3");
+		_vstr.push_back("Dark_ 4");
+		_vstr.push_back("Dark_ 5");
+		_vstr.push_back("Dark_ 6");
+		_vstr.push_back("Dark_ 7");
+		_vstr.push_back("Dark_ 8");
+		_vstr.push_back("Dark_ 9");
+		_vstr.push_back("Dark_10");
+		_vstr.push_back("Dark_11");
+		_vstr.push_back("Dark_12");
+		_vstr.push_back("Dark_13");
+		_vstr.push_back("Dark_14");
+		_vstr.push_back("Dark_15");
+		_vstr.push_back("Dark_16");
+		_vstr.push_back("Dark_17");
+		_vstr.push_back("Dark_18");
+		_vstr.push_back("Dark_19");
+		_vstr.push_back("Dark_20");
+		_vstr.push_back("Dark_21");
+		_vstr.push_back("Dark_22");
+		_vstr.push_back("Dark_23");
+		_vstr.push_back("Dark_24");
+		_vstr.push_back("Dark_25");
 	}
 public:
 	void AddItem(std::vector<std::string>& _vstr) //打光
---
+++ Decorator/MeasureItem/itemFactory.h
@@ -0,0 +1,24 @@
+#ifndef ITEMFACTORY_H
+#define ITEMFACTORY_H
+#include <string>
+#include "MiD25.h"
+#include "MiW9.h"
+
+class itemFactory
+{
+public:
+	MiItem* CreatItem(int intdex)
+	{
+		switch (intdex)
+		{
+		case 1:
+			return new W9();
+			break;
+		case 2:
+			return new D25();
+			break;
+		}
+	}
+	
+};
+#endif
