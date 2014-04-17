77818a4e38edc1775dcfc577b2587ecfbc330549
Kx_wang <Kx_wang@chilinopto.com.tw>
2014-04-17 11:56:16 +0800

    change main
    修改成動連連結

--- Adapter/sampleCode/main.cpp
+++ Adapter/sampleCode/main.cpp
@@ -1,12 +1,18 @@
 #include "target.h"
 #include "adapter.h"
+#include <vector>
+#include <iterator>
+
 int main()
 {
-	Target target;
-	target.Request();
+	std::vector<Target*> v;
+	v.push_back(new Target());
+	v.push_back(new Adapter());
 
-	Adapter adapter;
-	adapter.Request();
+	for (std::vector<Target*>::iterator it = v.begin(); it != v.end(); ++it)
+	{
+		(*it)->Request();
+	}
 
 	return 0;
 }
